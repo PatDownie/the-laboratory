83f5cabfc32a9defa3a9fe03324aa062fe6f859f
Patrick Downie <patdownie07@gmail.com>
2024-01-09 18:28:38 +0000

    worked on physics experiment

--- physics.js
+++ physics.js
@@ -1,5 +1,38 @@
-document.getElementById("body");
+const box = document.getElementById("physBody");
 
-function testy() {}
+console.log("working");
 
-window.addEventListener("onmousedown", testy);
+let boxCurrentY = 0;
+let boxPreviousY = 0;
+let boxCurrentX = 0;
+let boxPreviousX = 0;
+
+let boxIsFree = true;
+
+console.log(boxCurrentX);
+
+function grabBox() {
+  boxIsFree = false;
+  console.log(boxIsFree);
+}
+
+function freeBox() {
+  boxIsFree = true;
+  console.log(boxIsFree);
+}
+
+function drag(event) {
+  if (event.buttons === 1) {
+    box.style.top = `${event.y - 50}px`;
+    box.style.left = `${event.x - 50}px`;
+  }
+}
+
+function momentum() {
+  const boxNextY = boxCurrentY + (boxCurrentY - boxPreviousY);
+  const boxNextX = boxCurrentX + (boxCurrentX - boxPreviousX);
+}
+
+document.addEventListener("mousedown", grabBox);
+document.addEventListener("mouseup", freeBox);
+document.addEventListener("mousemove", drag);
