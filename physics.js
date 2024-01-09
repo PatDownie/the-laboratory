210144556066481e05c6cd519946183284c00fbc
Patrick Downie <patdownie07@gmail.com>
2024-01-09 21:34:13 +0000

    added to physics experiment

--- physics.js
+++ physics.js
@@ -1,4 +1,5 @@
 const box = document.getElementById("physBody");
+const bonkSound = document.getElementById("bonkSound");
 
 console.log("working");
 
@@ -25,14 +26,46 @@ function drag(event) {
   if (event.buttons === 1) {
     box.style.top = `${event.y - 50}px`;
     box.style.left = `${event.x - 50}px`;
+    console.log(`boxcurrentY = ${boxCurrentY}   boxcurrentX = ${boxCurrentX}`);
   }
 }
 
+let boxNextY = 0;
+let boxNextX = 0;
+
+const collisionDampening = 1;
+const gravity = 1;
+
 function momentum() {
-  const boxNextY = boxCurrentY + (boxCurrentY - boxPreviousY);
-  const boxNextX = boxCurrentX + (boxCurrentX - boxPreviousX);
+  if (boxIsFree === true) {
+    if (boxCurrentY <= 0 || boxCurrentY >= window.innerHeight - 100) {
+      boxNextY = boxCurrentY - (boxCurrentY - boxPreviousY) * collisionDampening;
+      bonkSound.cloneNode().play();
+    } else {
+      boxNextY = boxCurrentY + (boxCurrentY - boxPreviousY);
+    }
+    if (boxCurrentX <= 0 || boxCurrentX >= window.innerWidth - 100) {
+      boxNextX = boxCurrentX - (boxCurrentX - boxPreviousX) * collisionDampening;
+      bonkSound.cloneNode().play();
+    } else {
+      boxNextX = boxCurrentX + (boxCurrentX - boxPreviousX);
+    }
+    box.style.top = `${boxNextY}px`;
+    box.style.left = `${boxNextX}px`;
+    boxPreviousY = boxCurrentY;
+    boxPreviousX = boxCurrentX;
+    boxCurrentY = boxNextY;
+    boxCurrentX = boxNextX;
+  } else {
+    boxPreviousY = boxCurrentY;
+    boxPreviousX = boxCurrentX;
+    boxCurrentY = box.offsetTop;
+    boxCurrentX = box.offsetLeft;
+  }
 }
 
 document.addEventListener("mousedown", grabBox);
 document.addEventListener("mouseup", freeBox);
 document.addEventListener("mousemove", drag);
+
+interval = setInterval(momentum, 1);
